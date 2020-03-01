541fad77b14b123d064557a4a69615850f4e3ba4
Tellow Krinkle <tellowkrinkle@gmail.com>
2020-03-01 00:01:46 -0600

    Use internal sdlimage on non-steam linux
    GOG release forgot to bundle libpng yaay

--- .travis_run.sh
+++ .travis_run.sh
@@ -22,7 +22,13 @@ elif [ "$TRAVIS_OS_NAME" == "linux" ]; then
 		run.sh make
 	else
 		mkdir src/extlib/include src/extlib/lib
-		./configure
+		# Program seems to break (testing with Ubuntu 18.04) with internal SDL, so using external SDL
+		# On the other hand, GOG copy is missing libpng12 so on systems with a newer libpng the game doesn't work
+		# Fix that by using internal sdlimage
+		./configure --with-internal-sdl-image
+		cd src/extlib/src/SDL2_image-2.0.0
+		bash fix-timestamps.sh
+		cd ../../../..
 		LDFLAGS="-Wl,-rpath,\\\$\$ORIGIN/lib64:." make
 	fi
 else
